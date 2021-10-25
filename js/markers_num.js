cf0315bc609e56b7a9223e05308b26e2133a0e40
Noooahh <noah.heimann01@gmail.com>
2021-10-25 07:01:51 +0200

    test

--- js/markers_num.js
+++ js/markers_num.js
@@ -5,7 +5,7 @@ L.marker([-7230, 3988], {icon: cardIcon6}).addTo(map)
 L.marker([-6787.25, 4595.25], {icon: cardIcon2}).addTo(map)
   .bindPopup("<h2>Cypress FLats 6</h3><p><a href='https://i.imgur.com/uziiy8L.png' data-lightbox='image-1' data-title='Cypress FLats 6'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-5579.25, 2807], {icon: cardIcon3}).addTo(map)
-  .bindPopup("<h2>Moringwood 1</h3><p><a href='https://i.imgur.com/AEjqlHg.jpg' data-lightbox='image-1' data-title='Moringwood 1'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
+  .bindPopup("<h2>Moringwood 1</h3><p><a href='https://i.imgur.com/AEjqlHg.jpg' data-lightbox='image-1' data-title='Morningwood 1'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-7297.75, 3667.5], {icon: cardIcon6}).addTo(map)
   .bindPopup("<h2>Hafen 22</h3><p><a href='https://i.imgur.com/cloUVBa.png' data-lightbox='image-1' data-title='Hafen 22'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-7610, 6410], {icon: cardIcon7}).addTo(map)
@@ -16,3 +16,5 @@ L.marker([-1260, 3748.5], {icon: cardIcon1}).addTo(map)
   .bindPopup("<h2>Paleto Bay 3</h3><p><a href='https://i.imgur.com/dz8PAnv.jpg' data-lightbox='image-1' data-title='Paleto Bay 3'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-7481, 4296], {icon: cardIcon6}).addTo(map)
   .bindPopup("<h2>Hafen 6</h3><p><a href='https://i.imgur.com/YJ4pN8m.png' data-lightbox='image-1' data-title='Hafen 6'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
+L.marker([-4750, 3900], {icon: cardIcon3}).addTo(map)
+  .bindPopup("<h2>das test</h3><p><a href='https://i.imgur.com/p0ZZebp.png' data-lightbox='image-1' data-title='testo'>Open In-Game Screenshot</a><div class='input-group input-group-sm mb-3'></div></div></p>");
