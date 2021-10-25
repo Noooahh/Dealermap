9fbda959a1588351b963f15513240d5ddf3875b4
Noooahh <noah.heimann01@gmail.com>
2021-10-25 23:38:21 +0200

    coords

--- js/markers_num.js
+++ js/markers_num.js
@@ -14,13 +14,13 @@ L.marker([-1260, 3748.5], {icon: cardIcon1}).addTo(map)
   .bindPopup("<h2>Paleto Bay 3</h2><p><a href='https://i.imgur.com/3SnahoN.jpeg' data-lightbox='image-1' data-title='Paleto Bay 3'>Screenshot öffnen</a><div class='input-group input-group-sm mb-3'></div></div></p>");
 L.marker([-7481, 4296], {icon: cardIcon6}).addTo(map)
   .bindPopup("<h2>Hafen 6</h2><p><a href='https://i.imgur.com/4J7bdNy.jpeg' data-lightbox='image-1' data-title='Hafen 6'>Screenshot öffnen</a><div class='input-group input-group-sm mb-3'></div></div></p>");
-L.marker([-7481, 4296], {icon: cardIcon1}).addTo(map)
+L.marker([-3825, 3930], {icon: cardIcon1}).addTo(map)
   .bindPopup("<h2>Harmony 1</h2><p><a href='https://i.imgur.com/PSgsW1T.jpeg' data-lightbox='image-1' data-title='Harmony 1'>Screenshot öffnen</a><div class='input-group input-group-sm mb-3'></div></div></p>");
-L.marker([-7481, 4296], {icon: cardIcon1}).addTo(map)
+L.marker([-2020, 3435], {icon: cardIcon1}).addTo(map)
   .bindPopup("<h2>Sägewerk 1</h2><p><a href='https://i.imgur.com/npgsDEG.jpeg' data-lightbox='image-1' data-title='Sägewerk 1'>Screenshot öffnen</a><div class='input-group input-group-sm mb-3'></div></div></p>");
-L.marker([-7481, 4296], {icon: cardIcon2}).addTo(map)
+L.marker([-6615, 4913], {icon: cardIcon2}).addTo(map)
   .bindPopup("<h2>Mountain 8</h2><p><a href='https://i.imgur.com/fLlNWel.jpeg' data-lightbox='image-1' data-title='Mountain 8'>Screenshot öffnen</a><div class='input-group input-group-sm mb-3'></div></div></p>");
-L.marker([-7481, 4296], {icon: cardIcon6}).addTo(map)
+L.marker([-7675, 3954], {icon: cardIcon6}).addTo(map)
   .bindPopup("<h2>Hafen 14</h2><p><a href='https://i.imgur.com/VaXMr3j.jpeg' data-lightbox='image-1' data-title='Hafen 14'>Screenshot öffnen</a><div class='input-group input-group-sm mb-3'></div></div></p>");
-L.marker([-7481, 4296], {icon: cardIcon6}).addTo(map)
+L.marker([-7575, 4107], {icon: cardIcon6}).addTo(map)
   .bindPopup("<h2>Hafen 13</h2><p><a href='https://i.imgur.com/naURBlT.jpeg' data-lightbox='image-1' data-title='Hafen 13'>Screenshot öffnen</a><div class='input-group input-group-sm mb-3'></div></div></p>");
