9ced80e75a00021327f6198ddd8252d7ae316cfb
Noooahh <noah.heimann01@gmail.com>
2021-10-25 06:53:12 +0200

    .

---
+++ js/markers.js
@@ -0,0 +1,6 @@
+  L.marker([-2574.75, 6485.5], {icon: cardMarker}).addTo(map)
+    .bindPopup("Insel 1<br><a href='https://i.imgur.com/3sx9iLU.jpeg' data-lightbox='image-1' data-title='Insel 1'>Open In-Game Screenshot</a>");
+  L.marker([-617, 3907.25], {icon: cardMarker}).addTo(map)
+    .bindPopup("Insel 2<br><a href='http://i.epvpimg.com/ZbhMbab.png' data-lightbox='image-1' data-title='Insel 2'>Open In-Game Screenshot</a>");
+  L.marker([-7242.5, 5172.25], {icon: cardMarker}).addTo(map)
+    .bindPopup("Insel 3<br><a href='http://i.epvpimg.com/9bxweab.png' data-lightbox='image-1' data-title='Insel 3'>Open In-Game Screenshot</a>");
